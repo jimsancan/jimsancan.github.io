16319385c6a73b880de79c251258b575de022769
Jimmy Sanders-Cannon <jimmysancan@yahoo.com>
2018-07-22 13:32:44 -0400

    added base route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from '../src/components/app/App';
 import registerServiceWorker from './registerServiceWorker';
 import '../src/styles/main.scss'
 import 'bootstrap/dist/css/bootstrap.css'
+import {BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App/></Router>, document.getElementById('root'));
 registerServiceWorker();
